7801119229100c04f8b5ad38089f02d8c2299298
emwkb1ri <34179331+emwkb1ri@users.noreply.github.com>
2026-08-07 16:42:13 -0400

    changed Updater URL to my github repository.

--- Helpers/AppReleaseInfo.cs
+++ Helpers/AppReleaseInfo.cs
@@ -3,5 +3,5 @@ namespace NetKeyer.Helpers;
 public static class AppReleaseInfo
 {
     public const string Revision = "2.1.32";
-    public const string GitHubRepositoryUrl = "https://github.com/NetKeyer/NetKeyer";
+    public const string GitHubRepositoryUrl = "https://github.com/emwkb1ri/NetKeyer";
 }
